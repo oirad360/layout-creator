e4f36a28aff44b57d641eb0a0f647890a593be0d
Dario Anzalone <oirad36043@gmail.com>
2021-07-27 20:31:46 +0200

    prototipo pagina di vendita
    Prototipo di una tipica pagina di vendita personalizzata (definita in maniera statica tramite html+css)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,107 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel='stylesheet' href='/provaTesi/public/style/home.css'>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
-        ciao
+        <main>
+            <div class=row data-id=row1>
+                <section class=elem data-row=1 data-col=1>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                </section>
+                <section class=elem data-row=1 data-col=2>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                </section>
+                <section class=elem data-row=1 data-col=3>
+                </section>
+                <section class=elem data-row=1 data-col=4>
+                </section>
+            </div>
+            <div class=row  data-id=row2>
+                <section class=elem data-row=2 data-col=1>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                    <div class=product>
+                    </div>
+                </section>
+                <section class=elem data-row=2 data-col=2>
+                </section>
+                <section class=elem data-row=2 data-col=3>
+                </section>
+                <section class=elem data-row=2 data-col=4>
+                </section>
+            </div>
+            <div class=row  data-id=row3>
+                <section class=elem data-row=3 data-col=1>
+                </section>
+                <section class=elem data-row=3 data-col=2>
+                </section>
+                <section class=elem data-row=3 data-col=3>
+                </section>
+                <section class=elem data-row=3 data-col=4>
+                </section>
+            </div>
+            <div class=row  data-id=row4>
+                <section class=elem data-row=4 data-col=1>
+                </section>
+                <section class=elem data-row=4 data-col=2>
+                </section>
+                <section class=elem data-row=4 data-col=3>
+                </section>
+                <section class=elem data-row=4 data-col=4>
+                </section>
+            </div>
+        </main>
     </body>
 </html>
