9bbc54c2affd8aca3f9cf0dbfc8ae5d65f31308f
Dario Anzalone <oirad36043@gmail.com>
2021-08-12 17:11:31 +0200

    Create inserimenti database nuovo.sql

---
+++ inserimenti database nuovo.sql
@@ -0,0 +1,49 @@
+create table users(
+id integer primary key auto_increment,
+nome varchar(255) not null,
+cognome varchar(255) not null,
+username varchar(16) not null unique,
+email varchar(255) not null unique,
+password varchar(255) not null
+)engine = 'InnoDB';
+
+create table layouts(
+id integer primary key auto_increment,
+display varchar(255),
+flexDirection varchar(255),
+height varchar(255),
+width varchar(255)
+)engine='InnoDB';
+
+create table usersLayouts(
+    layout_id integer primary key,
+    foreign key(layout_id) references layouts(id) on update cascade on delete cascade,
+    index ind_layout_id(layout_id),
+    user_id integer not null,
+    foreign key(user_id) references users(id) on update cascade on delete cascade,
+    index ind_user_id(user_id),
+    unique(user_id,layout_id)
+)engine = 'InnoDB';
+
+create table childs(
+id integer primary key auto_increment,
+layout_id integer not null,
+foreign key(layout_id) references layouts(id) on update cascade on delete cascade,
+index ind_layout_id(layout_id),
+data_gen integer not null,
+data_id integer not null,
+data_parent_gen integer not null,
+data_parent_id integer not null,
+hasChilds boolean not null,
+title varchar(255),
+fontSize varchar(255),
+display varchar(255),
+flexDirection varchar(255),
+height varchar(255),
+width varchar(255),
+margin varchar(255)
+)engine='InnoDB';
+
+insert into users(nome,cognome,username,email,password) values('Dario','Anzalone','oirad360','darioanzalone@live.it','dario123');
+insert into users(nome,cognome,username,email,password) values('Mario','Rossi','mario99','mariorossi@live.it','mario123');
+
